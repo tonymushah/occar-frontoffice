300c01d0bf6b580fdb4ecce002abd3c17295b7f1
tonymushah <tonyramanantsalama@gmail.com>
2024-01-09 02:31:19 +0300

    fixed start script

--- app/components/RootNavbar.tsx
+++ app/components/RootNavbar.tsx
@@ -10,10 +10,6 @@ export default function RootNavBar(){
             <HStack spacing={4}>
                 <Link color={location.pathname == "/" ? "red.500" : undefined} marginLeft={4} as={NavLink} to={"/"}>Home</Link>
                 <Spacer/>
-                <Link as={NavLink} color={location.pathname.startsWith("/article") ? "red.500" : undefined} to={"/article"}>Articles</Link>
-                <Link as={NavLink} color={location.pathname.startsWith("/magasin") ? "red.500" : undefined} to={"/magasin"}>Magasin</Link>
-                <Link as={NavLink} color={location.pathname.startsWith("/unite-mesure") ? "red.500" : undefined} to={"/unite-mesure"}>Unite de mesure</Link>
-                <Link as={NavLink} color={location.pathname.startsWith("/etat-stock") ? "red.500" : undefined} to={"/etat-stock"}>Etat du Stock</Link>
                 <Box m={1}>
                     <ColorModeSwitcher/>
                 </Box>
